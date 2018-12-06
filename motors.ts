8abba602870a446333633e0b04df5d3dcbc003f2
Bentley Born <bentley@crcibernetica.com>
2018-12-06 14:31:04 -0600

    update

--- motors.ts
+++ motors.ts
@@ -9,6 +9,7 @@ namespace motors {
         /**
          * Set the directional pin where the motor is connected, defaults to P0.
          */
+        //% parts="motors"
         setDirPin(dirPin: DigitalPin): void {
             this.dirPin = dirPin;
             pins.digitalWritePin(this.dirPin, 0);
@@ -18,11 +19,22 @@ namespace motors {
         /**
          * Set the speed pin where the motor is connected, defaults to P0.
          */
+        //% parts="motors"
         setSpdPin(spdPin: DigitalPin): void {
             this.spdPin = spdPin;
             pins.digitalWritePin(this.spdPin, 0);
             // don't yield to avoid races on initialization
         }
+
+        // /**
+        //  * Set the speed of the motor
+        //  * @param speed is the speed of the motor
+        //  */
+        // //% blockId="motor_forward" block="%motor|forward %speed"
+        // //% parts="motors"
+        // forward(speed: number): void {
+        //     this.speed = speed;
+        // }
 
     }
 
@@ -32,6 +44,7 @@ namespace motors {
      * @param dirPin is the direction pin
      */
     //% blockId="motor_create" block="Create motor on speed pin %spdPin|and direction pin %dirPin"
+    //% parts="motors"
     //% blockSetVariable=motor
     export function create(spdPin: DigitalPin, dirPin: DigitalPin): Motor {
         let motor = new Motor();
