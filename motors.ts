8a4924770ebeae2403995ad9a1475471b6a676fb
Bentley Born <bentley@crcibernetica.com>
2018-12-07 11:35:09 -0600

    Update motors.ts

--- motors.ts
+++ motors.ts
@@ -1,11 +1,13 @@
 //% color=190 icon="\uf1b9" block="Motors"
 namespace motors {
    /**
-   * @param pin Choose pin number
+   * @param dirPin Choose direction pin
+   * @param spdPin Choose speed pin
    */
-   //% block="Motor forward pin %pin"
+   //% block="Motor forward direction pin %dirPin|speed pin %spdPin"
    //% blockGap=8
-   export function forward(pin: DigitalPin): void {
-       pin.digitalWrite(true);
+   export function forward(dirPin: DigitalPin, spdPin: AnalogOutPin): void {
+       dirPin.digitalWrite(true);
+       spdPin.analogWrite(1023);
    }
 }
