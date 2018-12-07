b96ef6a9f4968129d89512f8495e5a240dc0fb37
Bentley Born <bentley@crcibernetica.com>
2018-12-07 08:58:41 -0600

    Update motors.ts

--- motors.ts
+++ motors.ts
@@ -1,54 +1,10 @@
 //% color=190 icon="\uf1b9" block="Motors"
 namespace motors {
 
-    export class Motor {
-        spdPin: AnalogPin;
-        dirPin: DigitalPin;
-        speed: number;
-
-        /**
-         * Set the directional pin where the motor is connected, defaults to P0.
-         */
-        setDirPin(dirPin: DigitalPin): void {
-            this.dirPin = dirPin;
-            pins.digitalWritePin(this.dirPin, 0);
-            // don't yield to avoid races on initialization
-        }
-
-        /**
-         * Set the speed pin where the motor is connected, defaults to P0.
-         */
-        setSpdPin(spdPin: AnalogPin): void {
-            this.spdPin = spdPin;
-            pins.analogWritePin(this.spdPin, 0);
-            // don't yield to avoid races on initialization
-        }
-
-        /**
-         * Set the speed of the motor
-         * @param speed is the speed of the motor
-         */
-        //% blockId="motor_forward" block="%motor|forward %speed"
-        forward(speed: number): void {
-            this.speed = speed;
-            pins.analogWrite(this.spdPin, this.speed);
-            pins.digitalWrite(this.dirPin, HIGH);
-        }
-
-    }
-
-    /**
-     * Create a new Motor driver
-     * @param spdPin is the speed pin
-     * @param dirPin is the direction pin
-     */
-    //% blockId="motor_create" block="Create motor on speed pin %spdPin|and direction pin %dirPin"
-    //% blockSetVariable=motor
-    export function create(spdPin: AnalogPin, dirPin: DigitalPin): Motor {
-        let motor = new Motor();
-        motor.setDirPin(dirPin);
-        motor.setSpdPin(spdPin);
-        return motor;
-    }
- 
+   //%blockId=motor_forward block="Motor forward"
+   //%blockGap=8
+   export function forward(): void {
+       pins.digitalWrite(DigitalPin.PA01, HIGH);
+       pins.analogWrite(AnalogPin.PA02, 1023);
+   }
 }
