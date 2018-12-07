e383e82ae1097dfde4a3e5238b898ef7922cbdc9
Bentley Born <bentley@crcibernetica.com>
2018-12-07 09:56:39 -0600

    Update motors.ts

--- motors.ts
+++ motors.ts
@@ -4,6 +4,6 @@ namespace motors {
    //% block="Motor forward"
    //% blockGap=8
    export function forward(): void {
-       pins.A2.digitalWrite(true);
+       pins.digitalWritePin(DigitalPin.PA06, true);
    }
 }
