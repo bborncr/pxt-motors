9f45c9c26aa422ad77c179f28cfba6b91d419158
Bentley Born <bentley@crcibernetica.com>
2018-12-07 11:59:04 -0600

    Update motors.ts

--- motors.ts
+++ motors.ts
@@ -3,11 +3,18 @@ namespace motors {
    /**
    * @param dirPin Choose direction pin
    * @param spdPin Choose speed pin
+   * @param speed Choose motor speed (0-1023)
    */
-   //% block="Motor forward direction pin %dirPin|speed pin %spdPin"
+   //% block="Motor direction pin %dirPin|speed pin %spdPin| Chose speed %speed"
+   //% speed.shadow="speedPicker"
    //% blockGap=8
-   export function forward(dirPin: DigitalPin, spdPin: AnalogOutPin): void {
-       dirPin.digitalWrite(true);
-       spdPin.analogWrite(1023);
+   export function move(dirPin: DigitalPin, spdPin: AnalogOutPin, speed: number): void {
+      if(speed > 0){
+         dirPin.digitalWrite(true);
+         spdPin.analogWrite(speed);
+      } else if (speed < 0){
+         dirPin.digitalWrite(false);
+         spdPin.analogWrite(1023 - Math.abs(speed));
+      }
    }
 }
